198f9015e3f32ae9d9b1dabe26921b90d7e698ab
Lokesh_Patidar <bansiyalokesh29988@gmail.com>
2022-08-24 16:05:40 +0530

    added my footer to my home page

---
+++ component/footer.js
@@ -0,0 +1,64 @@
+function footer(){
+    return `<div id="footer">
+    <div>
+        <div>
+            <img src="https://logos-world.net/wp-content/uploads/2022/04/Harvest-Logo.png" alt="">
+         </div>
+         <div>
+            <div>
+                <p>Harvest</p>
+                <p>Home</p>
+                <p>Why Harvest?</p>
+                <p>Features</p>
+                <p>Pricing</p>
+                <p>Integrations</p>
+                <p>Apps</p>
+                <p>Security</p>
+            </div>
+            <div>
+                <p>Community</p>
+                <p>Customer stories</p>
+                <p>Resources</p>
+                <p>Webinars</p>
+                <p>Help & support</p>
+                <p>Integrate with Harvest</p>
+                <p>Time Well Spent</p>
+                <p>Time in This Time</p>
+            </div>
+            <div>
+                <p>Company</p>
+                <p>About us</p>
+                <p>Blog</p>
+                <p>Careers</p>
+                <p>Legal</p>
+                <p>Contact us</p>
+            </div>
+         </div>
+    </div>
+     
+    <div>
+        <div>
+            <p>We also make: <span>Harvest Forecast</span>  – the fast and simple way to schedule your team across projects.</p>
+        </div>
+    </div>
+
+    <div>
+        <div>
+            <p>© 2006-2022 Harvest</p>
+        </div>
+        <div>
+            <div>
+                <a href="#" class="fa fa-twitter"></a><span> Twitter</span>
+            </div>
+            <div>
+                <a href="#" class="fa fa-linkedin"></a><span> LinkedIn</span>
+            </div>
+            <div>
+                <a href="#" class="fa fa-instagram"></a><span> Instagram</span>
+            </div>
+        </div>
+    </div>
+</div>`;
+}
+
+export default footer();

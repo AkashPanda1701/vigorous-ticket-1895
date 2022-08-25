a7daf1960c07fe8d821bb766b199b0cb72cfae5a
Akash Kumar Panda <akashpanda1701@gmail.com>
2022-08-25 20:30:10 +0530

    Signup

---
+++ scripts/signup.js
@@ -0,0 +1,69 @@
+document.querySelector("form button").addEventListener("click", Register);
+
+let RegisterUser = JSON.parse(localStorage.getItem("RegisterUser")) || [];
+
+function Register() {
+  let form = document.querySelector("form");
+
+  class User {
+    constructor(first, last, company, email, password) {
+      this.firstName = first;
+      this.lastName = last;
+      this.companyName = company;
+      this.email = email;
+      this.password = password;
+    }
+  }
+
+  let userinfo = new User(
+    form.first.value,
+    form.last.value,
+    form.company.value,
+    form.email.value,
+    form.password.value
+  );
+  
+  if(userinfo.password.length<8){
+    alert("Password must be at least 8 characters");
+    return;
+  }
+
+  let str = "!@#$%^&*";
+  let flag = false;
+
+  for (let i = 0; i < str.length; i++) {
+    if ((userinfo.password).includes(str[i])) {
+      flag=true;
+    }
+  }
+
+if(flag){
+
+    let isRegistered = false;
+
+    for(let i=0;i<RegisterUser.length;i++) {
+
+        if(RegisterUser[i].email==userinfo.email){
+            isRegistered=true;
+            break;
+        }
+    }
+
+    if(isRegistered){
+        alert('User already registered');
+        return;
+    }else{
+
+        RegisterUser.push(userinfo);
+        
+        localStorage.setItem("RegisterUser", JSON.stringify(RegisterUser));
+        alert("User registration successful");
+    }
+
+    
+}else{
+    alert("Password must include special characters");
+    return;
+
+}
+}

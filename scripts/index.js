c4eb67b78d4c8ab5f4e0205cd508cdd6b336b837
Lokesh_Patidar <bansiyalokesh29988@gmail.com>
2022-08-27 13:27:58 +0530

    Added my last task of 5th day for Home Page

---
+++ scripts/index.js
@@ -0,0 +1,48 @@
+
+function defaultChange(){
+    document.getElementById("c1").style.backgroundColor = "orangered";
+    document.getElementById("c2").style.backgroundColor = "white";
+    document.getElementById("c3").style.backgroundColor = "white";
+    document.getElementById("c4").style.backgroundColor = "white";
+}
+defaultChange();
+
+function change1(){
+    document.getElementById("text").innerText = "We really focus on work-life balance in remote work… Time tracking reveals if somebody is overburdened and that becomes an immediate conversation."
+    document.getElementById("text2").innerText = "Noah Gedrich, Zehner";
+    document.getElementById("image").src = "https://www.getharvest.com/hs-fs/hubfs/homepage/home-photo-zehner.jpg?width=1400&height=935&name=home-photo-zehner.jpg";
+    document.getElementById("c1").style.backgroundColor = "orangered";
+    document.getElementById("c2").style.backgroundColor = "white";
+    document.getElementById("c3").style.backgroundColor = "white";
+    document.getElementById("c4").style.backgroundColor = "white";
+}
+
+function change2(){
+    document.getElementById("text").innerText = "We needed something that worked, something super simple, because we don’t want to have to teach people how to use it as part of onboarding.";
+    document.getElementById("image").src ="https://www.getharvest.com/hs-fs/hubfs/homepage/home-photo-dovetail.jpg?width=1400&height=935&name=home-photo-dovetail.jpg";
+    document.getElementById("text2").innerText = "Nick Frandsen, Dovetail";
+    document.getElementById("c1").style.backgroundColor = "white";
+    document.getElementById("c2").style.backgroundColor = "orangered";
+    document.getElementById("c3").style.backgroundColor = "white";
+    document.getElementById("c4").style.backgroundColor = "white";
+}
+
+function change3(){
+    document.getElementById("text").innerText = "Having an easy, clean way to track time allows us to focus on the tough engineering problems where we bring value to our clients.";
+    document.getElementById("image").src ="https://www.getharvest.com/hs-fs/hubfs/homepage/home-photo-cooper.jpg?width=1400&height=935&name=home-photo-cooper.jpg";
+    document.getElementById("text2").innerText = "Genevieve Laing, Cooper Perkins";
+    document.getElementById("c1").style.backgroundColor = "white";
+    document.getElementById("c2").style.backgroundColor = "white";
+    document.getElementById("c3").style.backgroundColor = "orangered";
+    document.getElementById("c4").style.backgroundColor = "white";
+}
+
+function change4(){
+    document.getElementById("text").innerText = "Harvest was key to helping me start my company – it allowed us to move from managing a lot of subcontractors to managing a staff quite easily.";
+    document.getElementById("image").src ="https://www.getharvest.com/hs-fs/hubfs/homepage/home-photo-luminary.jpg?width=1400&height=935&name=home-photo-luminary.jpg";
+    document.getElementById("text2").innerText = "Sara Holoubek, Luminary Labs";
+    document.getElementById("c1").style.backgroundColor = "white";
+    document.getElementById("c2").style.backgroundColor = "white";
+    document.getElementById("c3").style.backgroundColor = "white";
+    document.getElementById("c4").style.backgroundColor = "orangered";
+}
